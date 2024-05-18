f798b0f2a9ad35c17732d93c590e193d9a44c51b
M4TTH3 <matthewauyeung04@gmail.com>
2024-05-18 12:42:34 -0400

    setting up more

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,37 @@ from fastapi import FastAPI, status
 from propelauth_fastapi import init_auth, User
 from dotenv import load_dotenv
 from pathlib import Path
+from pymongo import MongoClient, server_api
 import os
 
+
+"""
+Setup auth function
+"""
 # Load the .env file in the environment
-load_dotenv(Path(".env"))
+if Path(".env").exists():
+    load_dotenv(Path(".env"))
 
 # user: User = Depends(auth.require_user) verifies and injects user from auth token
 auth = init_auth("https://25870074.propelauthtest.com", os.environ["AUTH_API_KEY"])
 
+
+"""
+Setup the db engine
+"""
+uri = os.environ["MONGO_CONNECTION_STRING"]
+
+# Create a MongoClient with a MongoClientOptions object to set the Stable API version
+client = MongoClient(uri, server_api=server_api.ServerApi(
+    version='1', strict=True, deprecation_errors=True))
+
+
+"""
+Routes
+"""
+
 app = FastAPI()
 
-
-app.get("/verify_user", status_code=status.HTTP_202_ACCEPTED)
+    
+    
 
