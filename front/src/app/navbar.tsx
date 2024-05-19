79755cc3a448fe9db340aae135db56280b80d2cf
Amol Venkataraman <amolven16@gmail.com>
2024-05-18 22:17:41 -0400

    Changed format

--- front/src/app/navbar.tsx
+++ front/src/app/navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useLogoutFunction, useRedirectFunctions } from '@propelauth/react'
-import { GetAuth } from './authinfo'
 
 
 export default function Navbar() {
@@ -9,19 +8,22 @@ export default function Navbar() {
   const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
 
   return (
-    <nav className="p-4">
-      <div className="container mx-auto">
+    <nav className="p-4 flex justify-between">
+      <div className="nav-element min-w-60">
         <a href="/" className="font-bold text-xl">HawkHacks2024</a>
-        <div className="flex space-x-4">
-          <button onClick={() => location.href = '/'} className="">Home</button>
-          <button onClick={() => redirectToLoginPage()} className="">Login</button>
-          <button onClick={() => redirectToSignupPage()} className="">Sign up</button>
-          <button onClick={() => redirectToAccountPage()} className="">Account</button>
-          <button onClick={() => logoutFunction(true)} className="">Logout</button>
-          <button onClick={() => location.href = '/annotate'} className="">Annotate</button>
-          <button onClick={() => location.href = '/admin'} className="">Admin</button>
-        </div>
-        <GetAuth />
+      </div>
+      <div className="flex space-x-4">
+        <button onClick={() => location.href = '/'} className="">Home</button>
+        <button onClick={() => redirectToLoginPage()} className="">Login</button>
+        <button onClick={() => redirectToSignupPage()} className="">Sign up</button>
+        <button onClick={() => redirectToAccountPage()} className="">Account</button>
+        <button onClick={() => logoutFunction(true)} className="">Logout</button>
+        <button onClick={() => location.href = '/annotate'} className="">Annotate</button>
+        <button onClick={() => location.href = '/admin'} className="">Admin</button>
+      </div>
+      <div className="nav-element min-w-60 flex justify-end">
+        {/* <GetAuth /> */}
+        You must be logged in
       </div>
     </nav>
   );
