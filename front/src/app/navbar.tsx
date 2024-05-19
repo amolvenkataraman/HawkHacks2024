8685825443c7728a88da8162933f4e1b7e59dc44
Amol Venkataraman <amolven16@gmail.com>
2024-05-18 22:27:22 -0400

    Updated Navbar

--- front/src/app/navbar.tsx
+++ front/src/app/navbar.tsx
@@ -1,10 +1,39 @@
 'use client'
 
-import { useLogoutFunction, useRedirectFunctions } from '@propelauth/react'
+import { useLogoutFunction, useRedirectFunctions, useAuthInfo } from '@propelauth/react'
 
+function UserInfo() {
+  const authInfo = useAuthInfo();
+  const logoutFunction = useLogoutFunction();
+  const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
+
+  if (authInfo.loading) {
+    return (
+      <div className="nav-element min-w-60 space-x-4 flex justify-end">
+        Loading...
+      </div>
+    )
+  } else if (authInfo.isLoggedIn) {
+    return (
+      <div className="nav-element min-w-60 space-x-4 flex justify-end">
+        Loading...
+        <div>Welcome, {authInfo.user.email}!</div>
+        <button onClick={() => redirectToAccountPage()} className="">Account</button>
+        <button onClick={() => logoutFunction(true)} className="">Logout</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="nav-element min-w-60 space-x-4 flex justify-end">
+        <button onClick={() => redirectToLoginPage()} className="">Login</button>
+        <button onClick={() => redirectToSignupPage()} className="">Sign up</button>
+      </div>
+    )
+  }
+  return "LOL"
+}
 
 export default function Navbar() {
-  const logoutFunction = useLogoutFunction();
   const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
 
   return (
@@ -14,17 +43,10 @@ export default function Navbar() {
       </div>
       <div className="flex space-x-4">
         <button onClick={() => location.href = '/'} className="">Home</button>
-        <button onClick={() => redirectToLoginPage()} className="">Login</button>
-        <button onClick={() => redirectToSignupPage()} className="">Sign up</button>
-        <button onClick={() => redirectToAccountPage()} className="">Account</button>
-        <button onClick={() => logoutFunction(true)} className="">Logout</button>
         <button onClick={() => location.href = '/annotate'} className="">Annotate</button>
         <button onClick={() => location.href = '/admin'} className="">Admin</button>
       </div>
-      <div className="nav-element min-w-60 flex justify-end">
-        {/* <GetAuth /> */}
-        You must be logged in
-      </div>
+        <UserInfo />
     </nav>
   );
 }
