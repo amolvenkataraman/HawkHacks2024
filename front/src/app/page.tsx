e7ea3ef2cad37c3bf32fdd8ec353f2b8bc961f29
Amol Venkataraman <amolven16@gmail.com>
2024-05-19 00:33:16 -0400

    Changed app name

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,29 +4,25 @@ function AnonymousSplash() {
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
       <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        HawkHacks 2024 Project
+        Welcome to DataSrc
       </h1>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-        <Link
+        <span
           className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/usage/first-steps"
-          target="_blank"
         >
-          <h3 className="text-2xl font-bold">First Steps →</h3>
+          <h3 className="text-2xl font-bold">First box</h3>
           <div className="text-lg">
             Annotate images or smth
           </div>
-        </Link>
-        <Link
+        </span>
+        <span
           className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-          href="https://create.t3.gg/en/introduction"
-          target="_blank"
         >
-          <h3 className="text-2xl font-bold">Documentation →</h3>
+          <h3 className="text-2xl font-bold">Second box</h3>
           <div className="text-lg">
             What else to put here
           </div>
-        </Link>
+        </span>
       </div>
     </div>
   )
